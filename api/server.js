1ed9ecd314fc0542642212f8b961d07f16b7ed0a
Justin Price <mr.price@Justins-iMac.local>
2020-03-02 23:18:56 -0500

    fixed server, built volunteers router and model

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const trainingsRouter = require('../trainings/trainings-router');
+const volunteersRouter = require('../volunteers/volunteers-router');
 
 const server = express();
 
@@ -12,5 +13,6 @@ server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/trainings', authenticate, trainingsRouter);
+server.use('/api/volunteers', authenticate, volunteersRouter);
 
 module.exports = server;
---
+++ volunteers/volunteers-model.js
@@ -0,0 +1,10 @@
+const db = require("../database/dbConfig");
+
+module.exports = {
+  find,
+};
+
+function find() {
+  return db("volunteers");
+}
+
