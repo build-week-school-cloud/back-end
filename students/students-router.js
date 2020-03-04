179234d15e684b826fd14736ec0cdf7b4fe7fbab
Justin Price <mr.price@Justins-iMac.local>
2020-03-04 18:02:07 -0500

    student get working

--- students/students-router.js
+++ students/students-router.js
@@ -10,8 +10,16 @@ router.get('/', (req, res) => {
     res.json(students);
   })
   .catch(err => {
-    res.status(500).json({ message: 'Failed to get students' });
+    res.status(500).json({ message: 'Failed to get classes' });
   });
 });
+
+// function availableClasses (req, res, next) {
+//   if (req.body.name && req.body.location && req.body.subject && req.body.time){
+//     next() 
+//   } else {
+//     res.status(400).json({message: "no classes found"})
+//   }
+// }
 
 module.exports = router;
